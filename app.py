25de46a94fdad6b8af969e92945340ab86c258d8
Alvin <alee38@cougarnet.uh.edu>
2024-11-14 17:06:27 -0600

    New Chocolate

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ chocolates = [
         "name": "White Chocolate",
         "image": "https://m.media-amazon.com/images/I/61-pg5u+9pL.jpg",
         "price": 6.49
+    },
+    {
+        "name": "Test Chocolate",
+        "image": "https://m.media-amazon.com/images/I/61-pg5u+9pL.jpg",
+        "price": 5.33
     }
 ]
 
